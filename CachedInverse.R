4c2802146b19ddca1898d7ea5b18f57f20cd8969
michaelfuchs64 <michaelfuchs64@gmail.com>
2017-04-24 19:49:21 -0400

    R programming assignment 2

--- CachedInverse.R
+++ CachedInverse.R
@@ -1,6 +1,8 @@
 
-cacheSolve <- function(X) {
+cacheSolve <- function(X) 
+## Returns inverse matrix either calculated or from cache     
 ## X - object returned by makeCacheMatrix()
+{
     M <- X$get_inverse()
     if(!is.null(M)) {
         message("Inverse matrix fom cache")
@@ -13,15 +15,27 @@ cacheSolve <- function(X) {
     M
 }
 
-makeCacheMatrix <- function(M = matrix()) {
-    M_INV <- NULL
+makeCacheMatrix <- function(M = matrix()) 
+## Constructor of an object containing a matrix, it's cached inverse, 
+## and set/get methods for both
+{   M_INV <- NULL
+    
+    ## Method initiating matrix data
     set <- function(x) {
         M <<- x
         M_INV <<- NULL
     }
+    
+    ## Method returning matrix data
     get <- function() M
+    
+    ## Initiate Inversed matrix data
     set_inverse <- function(m_inv) M_INV <<- m_inv
+    
+    ## Method returning initiated matrix data
     get_inverse <- function() M_INV
+    
+    ## Return the constructed object as a list
     list(set = set, get = get,
          set_inverse = set_inverse,
          get_inverse = get_inverse)
